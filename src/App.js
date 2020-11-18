dd37630d46a4a76fd558cc24b4e03133d5704ecc
Gabriel Andrés Mercado Rivera <gabriel.mercado@uao.edu.co>
2020-11-18 16:25:15 -0500

    Se agrega card de fondo negro con sus estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Content from './Components/Content/Content';
 import InvoicesPrices from './Components/Content/invoices-prices/InvoicesPrices';
+import BlackComponent from './Components/bottomComponent/blackComponent/blackComponent'
 import image from './Images/mac.svg'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Content />
       <InvoicesPrices price={"21.3k"} label={"$12,095 overdue"} progress={true}/>
       <InvoicesPrices price={"12,095"} label={"Overdue"} progress={false}/>
+      <BlackComponent label={"Work Anywhere with the Invoicer Mobile Apps "}/>
       <img src={image} alt="alt" />
     </div>
   );
