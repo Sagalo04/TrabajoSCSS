dd2b88a49a8c2688f61e8ec5c31d3f5dea95174c
Angélica Garcia <angelicagarcia@Usuarios-MacBook-Pro.local>
2020-11-18 16:29:32 -0500

    app hotfix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import Content from './Components/Content/Content';
-<<<<<<< HEAD
-import InvoicesPrices from './Components/Content/top/invoices-prices/InvoicesPrices';
-=======
-import InvoicesPrices from './Components/Content/invoices-prices/InvoicesPrices';
 import BlackComponent from './Components/bottomComponent/blackComponent/blackComponent'
->>>>>>> dd37630d46a4a76fd558cc24b4e03133d5704ecc
 import image from './Images/mac.svg'
 
 function App() {
   return (
     <div className="App">
       <Content />
-      <InvoicesPrices price={"21.3k"} label={"$12,095 overdue"} progress={true}/>
-      <InvoicesPrices price={"12,095"} label={"Overdue"} progress={false}/>
       <BlackComponent label={"Work Anywhere with the Invoicer Mobile Apps "}/>
       <img src={image} alt="alt" />
     </div>
