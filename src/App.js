d632adcecc02b82cfd4219a1c60dd9de60b8da6a
Angélica Garcia <angelicagarcia@Usuarios-MacBook-Pro.local>
2020-11-18 16:55:52 -0500

    new image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Content from './Components/Content/Content';
 import BlackComponent from './Components/bottomComponent/blackComponent/blackComponent'
 import image from './Images/mac.svg'
+import profile from './Images/profile.svg'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Content />
       <BlackComponent label={"Work Anywhere with the Invoicer Mobile Apps "}/>
       <img src={image} alt="alt" />
+      <img src={profile} alt="alt" />
     </div>
   );
 }
