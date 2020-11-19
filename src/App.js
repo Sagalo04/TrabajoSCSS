d6f02e438ff648838882d631437e23a72456f890
Gabriel Andrés Mercado Rivera <gabriel.mercado@uao.edu.co>
2020-11-18 22:06:09 -0500

    card de upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Content from './Components/Content/Content';
 import BlackComponent from './Components/bottomComponent/blackComponent/blackComponent'
+import InvoicesUpload from './Components/bottomComponent/uploadInvoces/uploadInvoces'
 import image from './Images/mac.svg'
 import profile from './Images/profile.svg'
 
@@ -9,6 +10,7 @@ function App() {
       <Content />
       <BlackComponent label={"Work Anywhere with the Invoicer Mobile Apps "}/>
       <img src={image} alt="alt" />
+      <InvoicesUpload label={"Upload Invoices"}/>
       <img src={profile} alt="alt" />
     </div>
   );
