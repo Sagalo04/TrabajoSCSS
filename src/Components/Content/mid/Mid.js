9f2cfdf3476b40fd5d90ccd47df4eff886dca684
Gabriel Andrés Mercado Rivera <gabriel.mercado@uao.edu.co>
2020-11-19 23:06:22 -0500

    se agrega el grid

--- src/Components/Content/mid/Mid.js
+++ src/Components/Content/mid/Mid.js
@@ -10,17 +10,16 @@ function Mid(props) {
         <div className={Styles.mid}>
             <div className={Styles.profit}>
                 <TopHeader icon={[BARSCIRCLE]} title={"Total profit"} subtitle={"September 2020"} />
-                <div className={Styles.Deck}>
-                    <CardProfit profits={22.3} />
-                </div>
+
             </div>
-            <div>
-                <TopHeader icon={[ARROW]} title={"Expenses"} subtitle={""} />
-                <div className={Styles.Deck}>
-                    <CardExpenses bg={"#F0F4FF"} icon={ATLASSIAN} label={"Software"} price={20} date={"23 Sep, 2020"} />
-                    <CardExpenses bg={"#EFFCEF"} icon={MORE} label={"Management"} price={30} date={"26 Sep, 2020"} />
-                    <CardExpenses bg={"#E6F5FA"} icon={APPLE} label={"Support"} price={60} date={"28 Sep, 2020"} />
-                </div>
+            <TopHeader icon={[ARROW]} title={"Expenses"} subtitle={""} />
+            <div className={Styles.Deck}>
+                <CardProfit profits={22.3} />
+            </div>
+            <div className={Styles.Deck}>
+                <CardExpenses bg={"#F0F4FF"} icon={ATLASSIAN} label={"Software"} price={20} date={"23 Sep, 2020"} />
+                <CardExpenses bg={"#EFFCEF"} icon={MORE} label={"Management"} price={30} date={"26 Sep, 2020"} />
+                <CardExpenses bg={"#E6F5FA"} icon={APPLE} label={"Support"} price={60} date={"28 Sep, 2020"} />
             </div>
         </div>
     );
