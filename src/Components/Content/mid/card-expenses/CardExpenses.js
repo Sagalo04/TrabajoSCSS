f3507f64371b2e296290acdb483dac7065d4db8c
Santiago Garcna <santiago.garcna@uao.edu.co>
2020-11-23 17:03:18 -0500

    card fix

--- src/Components/Content/mid/card-expenses/CardExpenses.js
+++ src/Components/Content/mid/card-expenses/CardExpenses.js
@@ -4,18 +4,20 @@ import BackCard from './BackCard/BackCard';
 import clip from '../../../../Images/clip.svg'
 
 
-function CardExpenses({ bg,icon,label,price,date }) {
+function CardExpenses({ bg, icon, label, price, date }) {
     return (
         <div className={Styles.cardBody}>
             <BackCard bg={bg} className={Styles.backcard} />
             <div className={Styles.info}>
-                <div className={Styles.top}>
-                    <p className={Styles.price}>{`$${price}`}<label className={Styles.cents}>.00</label></p>
-                    <img src={clip} alt="alt"/>
+                <div>
+                    <div className={Styles.top}>
+                        <p className={Styles.price}>{`$${price}`}<label className={Styles.cents}>.00</label></p>
+                        <img src={clip} alt="alt" />
+                    </div>
+                    <p>{date}</p>
                 </div>
-                <p>{date}</p>
                 <div className={Styles.iconcont}>
-                    <img src={icon} alt="alt"/>
+                    <img src={icon} alt="alt" />
                     <p>{label}</p>
                 </div>
             </div>
